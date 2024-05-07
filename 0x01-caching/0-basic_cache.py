81bfcc5d10527e33ec9d3169972b6d69531706ab
kennyChukwuebuka <kenkinado@gmail>
2024-05-07 19:39:41 +0100

    updated

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,56 +3,35 @@
     Create a class BasicCache that inherits from
     BaseCaching and is a caching system
         Requirement:
-            use self.cache_data dictionary
+            use self.cache_data
 """
 
 from base_caching import BaseCaching
 
 
-class BaseCaching:
-    def __init__(self):
-        """
-        Initializes the object with an empty cache_data dictionary.
-
-        :param self: The object instance.
-        :return: None
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """
-        Print the current cache.
-
-        This function prints the contents of the cache_data
-        dictionary in a formatted way. It iterates over the
-        keys of the dictionary and prints each key-value
-        pair in the format "{key}: {value}".
-
-        Parameters:
-            self (BasicCache): The instance of the BasicCache class.
-
-        Returns:
-            None
-        """
-        print("Current cache:")
-        for key in self.cache_data:
-            print(f"{key}: {self.cache_data[key]}")
-
-
 class BasicCache(BaseCaching):
-    def put(self, key, item):
+    """ BasicCache defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ Initiliaze
         """
-        Add an item in the cache
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = list(self.cache_data.keys())[0]
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
 
     def get(self, key):
+        """ Get an item by key
         """
-        Get an item by key
-        :param key: Key to retrieve the item from the cache
-        :return: The item associated with the key if found, otherwise None
-        """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
