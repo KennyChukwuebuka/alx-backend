15fe47547c7fd1a49414a70b50388683b2fb286c
kennyChukwuebuka <kenkinado@gmail>
2024-05-07 15:50:14 +0100

    implemented base caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python3
 """
-    basic cache module
+    Create a class BasicCache that inherits from
+    BaseCaching and is a caching system
+        Requirement:
+            use self.cache_data
+        
 """
 
+from base_caching import BaseCaching
 
-BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = list(self.cache_data.keys())[0]
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
