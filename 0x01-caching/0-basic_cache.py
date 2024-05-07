1d46bdb7c6da91b6d70b5d5ca09886b477aaf84b
kennyChukwuebuka <kenkinado@gmail>
2024-05-07 19:37:02 +0100

    updated

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,29 +9,50 @@
 from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """ BasicCache defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
+class BaseCaching:
     def __init__(self):
-        """ Initiliaze
         """
-        super().__init__()
+        Initializes the object with an empty cache_data dictionary.
 
+        :param self: The object instance.
+        :return: None
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """
+        Print the current cache.
+
+        This function prints the contents of the cache_data
+        dictionary in a formatted way. It iterates over the
+        keys of the dictionary and prints each key-value
+        pair in the format "{key}: {value}".
+
+        Parameters:
+            self (BasicCache): The instance of the BasicCache class.
+
+        Returns:
+            None
+        """
+        print("Current cache:")
+        for key in self.cache_data:
+            print(f"{key}: {self.cache_data[key]}")
+
+
+class BasicCache(BaseCaching):
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item in the cache
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discard = list(self.cache_data.keys())[0]
-                del self.cache_data[discard]
-                print("DISCARD: {}".format(discard))
 
     def get(self, key):
-        """ Get an item by key
         """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        Get an item by key
+        :param key: Key to retrieve the item from the cache
+        :return: The item associated with the key if found, otherwise None
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
