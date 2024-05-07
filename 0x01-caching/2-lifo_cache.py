d6459202326ef40094eea64020f7bc346e6db257
kennyChukwuebuka <kenkinado@gmail>
2024-05-07 21:17:28 +0100

    updated

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+    create a class LIFOCache that inherits from BaseCaching
+        Requirement:
+            use self.cache_data
+"""
+
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.pop(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = next(iter(self.cache_data))
+                print("DISCARD: {}".format(discarded_key))
+                self.cache_data.popitem(last=False)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
