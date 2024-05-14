1ac31f7471169837f13a3875134b8447a1260a58
kennyChukwuebuka <kenkinado@gmail>
2024-05-14 16:30:01 +0100

    added app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    Flask App
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+        Index
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
