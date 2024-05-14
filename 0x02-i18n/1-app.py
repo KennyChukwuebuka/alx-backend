2153cff698620a73972b988adae17ed07c3a623c
kennyChukwuebuka <kenkinado@gmail>
2024-05-14 20:10:51 +0100

    added babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -25,3 +25,15 @@ class Config:
 
 babel = Babel(app)
 app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=True)
+def index():
+    """
+        Index
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
